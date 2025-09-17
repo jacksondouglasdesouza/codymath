972277f70a8ff95629ff571645a6ceb226be2a7c
jacksondouglasdesouza <jacksondouglasdesouza@hotmail.com>
2025-09-17 15:58:44 -0300

    fix: correct export path for algebra module

--- src/algebra/index.ts
+++ src/algebra/index.ts
@@ -1,7 +1,7 @@
 // src/algebra/index.ts
-
 import * as arithmeticFuncs from './arithmetic';
 
+// Isso cria o objeto 'arithmetic' que você quer exportar. Está perfeito.
 export const arithmetic = {
     ...arithmeticFuncs
 };
